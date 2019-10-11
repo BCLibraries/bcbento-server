47c9b70c44830f8954dc5e019e9b701360291f3e
Benjamin Florin <benjamin.florin@gmail.com>
2019-10-11 12:03:40 -0400

    Better error checking in deploy

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+# Deploys bcbento-server.
+#
+# Synopsis:
+#
+#     deploy.sh [branch-name]
+#
+
 # Branch to pull from
 BRANCH=${1:-master}
 
@@ -18,19 +25,26 @@ NEW_RELEASE=${RELEASES_DIR}/${RIGHT_NOW}-${BRANCH}
 SHARED_DIR=${APP_BASE}/shared
 LOG_DIR=${SHARED_DIR}/log
 
+# Something went wrong.
+error_exit()
+{
+	echo "$1" 1>&2
+	exit 1
+}
+
 # Pull the latest commit from master
-git clone ${REPO} -b ${BRANCH} ${NEW_RELEASE}
-cd ${NEW_RELEASE}
-find .git -type f -exec chmod 644 {} \;
+git clone ${REPO} -b ${BRANCH} ${NEW_RELEASE} || error_exit "Could not copy clone release"
+cd ${NEW_RELEASE} || error_exit "Could not cd to release"
+find .git -type f -exec chmod 644 {} \; || error_exit "Could not set git permissions"
 
 # Load the local environment variables
-cp ${SHARED_DIR}/.env.local ${NEW_RELEASE}
+cp ${SHARED_DIR}/.env.local ${NEW_RELEASE} || error_exit "Could not copy local .env"
 
 # Install
-APP_ENV=prod composer install --no-dev --optimize-autoloader
+APP_ENV=prod composer install --no-dev --optimize-autoloader || error_exit "Composer install failed"
 
 # Flush the redish cache
-redis-cli FLUSHALL
+redis-cli FLUSHALL || error_exit "Could not flush redis cache"
 
 # Re-use existing log
 rm -r ${NEW_RELEASE}/var/log
@@ -38,7 +52,7 @@ ln -s ${LOG_DIR} ${NEW_RELEASE}/var/log
 
 # Replace old version with new version
 unlink ${APP_BASE}/current
-ln -s ${NEW_RELEASE} ${APP_BASE}/current
+ln -s ${NEW_RELEASE} ${APP_BASE}/current || error_exit "Could not link new release to current"
 
 # Keep the last 3 releases
 if [[ `ls -1 ${RELEASES_DIR} 2>/dev/null | wc -l ` -gt 3 ]];
@@ -47,3 +61,6 @@ then
 	echo "Removing $(ls -t | tail -1)"
 	rm -rf "$(ls -t | tail -1)"
 fi
+
+# Restart PHP.
+sudo service php-fpm restart

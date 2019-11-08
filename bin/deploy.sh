21f5d3404c7be2b7afc5c298cc40ff1578c5e8a8
Benjamin Florin <benjamin.florin@gmail.com>
2019-11-08 13:59:13 -0500

    Various fixes to deploy script

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -10,7 +10,7 @@ REPO=https://github.com/BCLibraries/bcbento-server
 APP_BASE=/apps/bcbento-server
 
 # The new release
-RIGHT_NOW=`date +%Y-%m-%d-%H%M%S`
+RIGHT_NOW=$(date +%Y-%m-%d-%H%M%S)
 RELEASES_DIR=${APP_BASE}/releases
 NEW_RELEASE=${RELEASES_DIR}/${RIGHT_NOW}-${BRANCH}
 
@@ -19,12 +19,12 @@ SHARED_DIR=${APP_BASE}/shared
 LOG_DIR=${SHARED_DIR}/log
 
 # Pull the latest commit from master
-git clone ${REPO} -b ${BRANCH} ${NEW_RELEASE}
-cd ${NEW_RELEASE}
+git clone ${REPO} -b "${BRANCH}" "${NEW_RELEASE}"
+cd "${NEW_RELEASE}" || exit
 find .git -type f -exec chmod 644 {} \;
 
 # Load the local environment variables
-cp ${SHARED_DIR}/.env.local ${NEW_RELEASE}
+cp ${SHARED_DIR}/.env.local "${NEW_RELEASE}"
 
 # Install
 APP_ENV=prod composer install --no-dev --optimize-autoloader
@@ -33,17 +33,17 @@ APP_ENV=prod composer install --no-dev --optimize-autoloader
 redis-cli FLUSHALL
 
 # Re-use existing log
-rm -r ${NEW_RELEASE}/var/log
-ln -s ${LOG_DIR} ${NEW_RELEASE}/var/log
+rm -r "${NEW_RELEASE}"/var/log
+ln -s ${LOG_DIR} "${NEW_RELEASE}"/var/log
 
 # Replace old version with new version
 unlink ${APP_BASE}/current
-ln -s ${NEW_RELEASE} ${APP_BASE}/current
+ln -s "${NEW_RELEASE}" ${APP_BASE}/current
 
 # Keep the last 3 releases
-if [[ `ls -1 ${RELEASES_DIR} 2>/dev/null | wc -l ` -gt 3 ]];
+if [[ $(ls -1 ${RELEASES_DIR} 2>/dev/null | wc -l ) -gt 3 ]];
 then
-	cd ${RELEASES_DIR}
+	cd ${RELEASES_DIR} || exit
 	echo "Removing $(ls -t | tail -1)"
 	rm -rf "$(ls -t | tail -1)"
 fi

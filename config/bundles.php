37b31808d8210c18d975707b7682f3962d171043
Benjamin Florin <benjamin.florin@gmail.com>
2023-02-02 13:57:38 -0500

    Add CORS support

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,5 @@ return [
     TheCodingMachine\GraphQLite\Bundle\GraphQLiteBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
 ];
